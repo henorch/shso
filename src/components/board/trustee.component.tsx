e35e8a44a9ee2d591d8471daa838e88daf570aa5
henorch <henorch01@gmail.com>
2024-05-18 15:37:21 +0100

    modified

--- src/components/board/trustee.component.tsx
+++ src/components/board/trustee.component.tsx
@@ -7,52 +7,54 @@ type TrusteeProps = {
 }
 
 const NextButton = () => {
-    return(
+    return (
         <div style={{
-            position:"absolute",
-            zIndex:1,
-            background:"white",
-            padding:"10px",
-            top:1000,
-            opacity:0.5,
-            borderRadius:"50%",
+            position: "absolute",
+            zIndex: 1,
+            background: "white",
+            padding: "10px",
+            top: 1000,
+            opacity: 0.5,
+            borderRadius: "50%",
             marginRight: "20px",
-            right:10,
+            right: 10,
         }}>
-           <AiOutlineArrowRight/>
+            <AiOutlineArrowRight />
         </div>
     )
 }
 
 
-const Trusteed = ({trustees}: TrusteeProps) : JSX.Element => {
-   return <>
-        <NextButton/>
-   {trustees.map((trust, index) => <div
-    
-    key={index}
-    style={{
-        minWidth:"260px",
-        position:"relative",
-        borderRadius:10,
-        height:"400px",
-        background:`url(${trust.image})`,
-        backgroundPosition:"center",
-        border:"1px solid black",
-        margin:"5px",
-        zIndex:-1,
-   }}>
-<div style={{
-    background:"white",
-    position:'absolute',
-    bottom:0,
-    textAlign:"center",
-    opacity:0.5,
-    width:"100%"
-}}>
-    <b>{trust.name}</b>
-   <p>{trust.status}</p>
-   </div>
-   </div>)}</>
+const Trusteed = ({ trustees }: TrusteeProps): JSX.Element => {
+    return <>
+        <NextButton />
+        {trustees.map((trust, index) => <div
+
+            key={index}
+            style={{
+                minWidth: "150px",
+                position: "relative",
+                borderRadius: 10,
+                height: "250px",
+                backgroundImage: `url(${trust.image})`,
+                backgroundSize: 'cover',
+                backgroundRepeat: 'no-repeat',
+                backgroundPosition: "center",
+                border: "1px solid black",
+                margin: "5px",
+                zIndex: -1,
+            }}>
+            <div style={{
+                background: "white",
+                position: 'absolute',
+                bottom: 0,
+                textAlign: "center",
+                opacity: 0.5,
+                width: "100%"
+            }}>
+                <b>{trust.name}</b>
+                <p>{trust.status}</p>
+            </div>
+        </div>)}</>
 }
 export default Trusteed
--- src/footer/footer.component.tsx
+++ src/footer/footer.component.tsx
@@ -1,43 +1,118 @@
 import { Link } from "react-router-dom"
+const footerLink = [
+    {
+        id: 0,
+        title: 'Home',
+        link: '/'
+    },
+    {
+        id: 1,
+        title: 'about us',
+        link: '/about_us'
+    },
+    {
+        id: 2,
+        title: 'Membership',
+        link: '/member'
+    },
+    {
+        id: 3,
+        title: 'Grants/Donation',
+        link: '/grant_don'
+    },
+    {
+        id: 4,
+        title: 'Meeting & Minutes',
+        link: '/m_andm'
+    },
+    {
+        id: 5,
+        title: 'Job Offer',
+        link: '/job_offers'
+    },
+
+
+]
+
+
+type LinkProps = {
+    id: number,
+    title: String,
+    link: String
+}
+
+type linkPropsType = {
+    linkProp: LinkProps
+}
+
+
+const FooterLink = () => {
+    return <>
+        <ul style={{
+            listStyleType: "none"
+        }}>{footerLink.map((link) => <li><Link style={{
+            color: 'white'
+        }} to={link.link}>{link.title}</Link></li>)}</ul>
+    </>
+}
+
+const OurCommunity = () => {
+    return (
+        <h3>You can Follow us:</h3>
+    )
+}
+const ContactUs = () => {
+    return (
+        <div>
+            <h4>Contact us</h4>
+            <sub>Please enter your message below</sub>
+            <form>
+                <table>
+                    <tr>
+                        <td>Name</td>
+                        <td><input placeholder="enter your name" /></td>
+                    </tr>
+                    <tr>
+                        <td>title</td>
+                        <td><input placeholder="enter title" /></td>
+                    </tr>
+                    <tr>
+                        <td>message</td>
+                        <td><textarea style={{
+                            height: '50px'
+                        }} aria-multiline placeholder="enter yout message" /></td>
+                    </tr>
+                    <tr><td colSpan={2}><button type="submit">Submit</button></td></tr>
+                </table>
+            </form>
+        </div>
+    )
+}
 
 const Footer = () => {
     return (
         <div style={{
-            minHeight: "200px",
             width: "100%",
             background: "black",
-            color: "white"
+            color: "white",
+            display: "flex",
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
         }}>
-
-            <ul style={{
-                listStyleType: 'none'
+            <div style={{
+                minHeight: "200px",
+                width: "100%",
+                display: "flex",
+                justifyContent: 'space-around',
+                flexDirection: 'row',
+                flexWrap: 'wrap',
             }}>
-                <li><Link style={{
-                    color: 'white'
-                }} to="/">Home</Link></li>
-                <li><Link style={{
-                    color: 'white'
-                }} to="/">Press Release</Link></li>
-                <li><Link style={{
-                    color: 'white'
-                }} to="/">Membership</Link></li>
-                <li><Link style={{
-                    color: 'white'
-                }} to="/">Donation/Grant</Link></li>
-                <li><Link style={{
-                    color: 'white'
-                }} to="/">Upcoming</Link></li>
-                <li><Link style={{
-                    color: 'white'
-                }} to="/">@ a Glance</Link></li>
-                <li><Link style={{
-                    color: 'white'
-                }} to="/">Job OPffer</Link></li>
-                <li><Link style={{
-                    color: 'white'
-                }} to="/">Meeting & Minutes</Link></li>
-
-            </ul>
+                <OurCommunity />
+                <FooterLink />
+                <ContactUs />
+            </div>
+            copyright &c by Henovative Global Concept
         </div>
     )
 }
